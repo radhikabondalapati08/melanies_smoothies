9acc74bff8973e567b16f2de9560ffd76a74b8c0
radhikabondalapati08 <radhikabondalapati08@gmail.com>
2025-09-14 23:13:17 -0400

    Update streamlit_app.py
    insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,10 +42,9 @@ if ingredients_list:
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
     st.write(ingredients_string)
   
-        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + '""" ,'""" + name_on_order + """')"""
-    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            #values ('""" + ingredients_string + """ ','"""+name_on_order + """')"""
+   
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """ ','"""+name_on_order + """')"""
 
     #st.write(my_insert_stmt)
     #st.stop()
