6bb7f140663e53917461bd281baadecb52e48dfe
radhikabondalapati08 <radhikabondalapati08@gmail.com>
2025-09-14 22:23:55 -0400

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
-pd_df=mydataframe.to_pandas()
+pd_df = mydataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
